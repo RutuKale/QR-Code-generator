17e71b580e857c5a04d830d69434cb9416acb77c
RutuKale <rutujakale2112@gamil.com>
2024-11-23 11:45:06 +0530

    Initialize project using Create React App

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from ;
+
+const Main = () => {
+  return (
+    <div className={style.container}>
+        <h1>QR Code Generator</h1>
+    </div>
+  )
+}
+
+export default Main;
